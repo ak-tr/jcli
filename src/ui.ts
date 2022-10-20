4419d6b94607bc42436e24ff3b050ecc8a7b9bda
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-10-20 21:48:26 +0100

    ✨ added holding ctrl for fast navigation

--- src/ui.ts
+++ src/ui.ts
@@ -35,11 +35,18 @@ export class UI {
     // Set inital content
     infoBar.setContent(getInfoBarContentString(this.listIndex, this.fileName))
 
+    // Focus list on initial load
     list.focus();
-    list.key(["up", "down"], (_ch, key) => {
-      this._updateListIndex(key.name);
+
+    // Special key functions
+    list.key(["up", "down", "C-down", "C-up"], (_ch, key) => {
+      this._updateListIndex(key.name, key.ctrl, list);
       infoBar.setContent(getInfoBarContentString(this.listIndex, this.fileName))
       this.screen.render();
+    });
+
+    list.on("select", (item) => {
+      this.screen.log(list.getItemIndex(item), list.getItem(item).content);
     });
 
     [list, infoBar].forEach((widget) => this.screen.append(widget));
@@ -47,18 +54,37 @@ export class UI {
     this.screen.render();
   }
 
-  _updateListIndex = (keyEvent: string) => {
-    // If already at top and key up don't change index
+  _updateListIndex = (keyEvent: string, ctrl: boolean, list: blessed.Widgets.ListElement) => {
+    let offset = 0;
+
     if ((this.listIndex == 1) && (keyEvent == Key.Up)) {
-      return
+      return this.listIndex;
     }
 
     // If EOF and key down don't update index
     if ((this.listIndex == this.maxIndex) && (keyEvent == Key.Down)) {
-      return
+      return this.listIndex;
     }
 
-    this.listIndex += keyEvent == Key.Up ? -1 : 1;
+    // If less than 10 lines remaining to the end and ctrl is held, go to end
+    if ((keyEvent == Key.Down) && (this.listIndex >= this.maxIndex - 11) && ctrl) {
+      list.select(this.maxIndex - 1);
+      return this.listIndex = this.maxIndex;
+    }
+
+    // If less than 10 lines remaining to the beginning, go to end
+    if ((keyEvent == Key.Up) && (this.listIndex <= 11) && ctrl) {
+      list.select(0);
+      return this.listIndex = 1;
+    }
+
+    if (ctrl) {
+      // Value 9 because single key press already makes offset 1
+      offset = keyEvent == Key.Down ? 9 : -9;
+      list.move(offset);
+    }
+
+    return this.listIndex += (keyEvent == Key.Up ? -1 : 1) + offset;
   }
 }
 
